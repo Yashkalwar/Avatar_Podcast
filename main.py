e1b2ac0d0e09caef2f18a975b07f70264c357b8c
Arihant Kamdar <59435569+arihantkamdar@users.noreply.github.com>
2025-06-28 13:26:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from arvix.core import run_ai_arxiv_search
+from typing import List
+
+app = FastAPI()
+
+class ArxivQuery(BaseModel):
+    prompt: str
+    max_results: int = 3
+
+@app.post("/search/")
+def search_arxiv_api(query: ArxivQuery):
+    papers = run_ai_arxiv_search(query.prompt, max_results=query.max_results)
+
+    # Script Generations
+    # script_generation = 
+
+    return { "results": papers}
